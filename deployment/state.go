e1d11c782fe0a0d1615b9d01e2b70ffb05f924c0
AnthonyTGoh <anthony_goh@mednax.com>
2025-04-14 11:27:06 -0400

    Attempt more OmitEmpty for new state file issues

--- deployment/state.go
+++ deployment/state.go
@@ -5,11 +5,11 @@ type TerraformState struct {
 }
 
 type StateResource struct {
-	Module    interface{}              `json:"module"`
-	Mode      interface{}              `json:"mode"`
-	Type      interface{}              `json:"type"`
-	Name      interface{}              `json:"name"`
-	Provider  interface{}              `json:"provider"`
+	Module    interface{}              `json:"module,omitempty"`
+	Mode      interface{}              `json:"mode,omitempty"`
+	Type      interface{}              `json:"type,omitempty"`
+	Name      interface{}              `json:"name,omitempty"`
+	Provider  interface{}              `json:"provider,omitempty"`
 	Instances []*StateResourceInstance `json:"instances"`
 }
 
@@ -20,7 +20,7 @@ type StateResourceInstance struct {
 
 type StateResourceAttributes struct {
 	Id       interface{}            `json:"id"`
-	Location interface{}            `json:"location"`
-	Name     interface{}            `json:"name"`
-	Tags     map[string]interface{} `json:"tags"`
+	Location interface{}            `json:"location,omitempty"`
+	Name     interface{}            `json:"name,omitempty"`
+	Tags     map[string]interface{} `json:"tags,omitempty"`
 }
