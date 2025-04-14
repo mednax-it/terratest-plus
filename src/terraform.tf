ac632adf3c5e5fa17921de38cb3c314996bf812a
AnthonyTGoh <anthony_goh@mednax.com>
2025-04-14 10:33:48 -0400

    New tStorage Account Name

--- src/terraform.tf
+++ src/terraform.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "sdbitempstoragedeleteme123"
+  name                     = "sdbideletemetemp"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
